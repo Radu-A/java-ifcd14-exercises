862c0cb41c29d6458c9ba2b2961ab2aabe7d72f7
Radu-A <elcastravete@gmail.com>
2026-04-14 20:58:23 +0200

    ejercicio13: finish

---
+++ src/boletin01/Ejercicio12.java
@@ -0,0 +1,13 @@
+package boletin01;
+
+public class Ejercicio12 {
+
+	public static void main(String[] args) {
+		int num = 0;
+		for (int i = 20; i <= 27; i++) {
+			num = i - 19;
+			System.out.printf("Número %d: %d%n", num, i);
+		}
+	}
+
+}
---
+++ src/boletin01/Ejercicio13.java
@@ -0,0 +1,13 @@
+package boletin01;
+
+public class Ejercicio13 {
+
+	public static void main(String[] args) {
+		int square = 0;
+		for (int i = 1; i <=7; i++) {
+			square = i*i;
+			System.out.printf("El cuadrado de %d es %2d%n", i, square);
+		}
+	}
+
+}
