ce3e13a9756e7d12b53fa4a052ad0fb4eec47dd5
Radu-A <elcastravete@gmail.com>
2026-04-16 09:30:39 +0200

    b02e12: start

--- src/boletin02/Ejercicio12.java
+++ src/boletin02/Ejercicio12.java
@@ -4,11 +4,27 @@ public class Ejercicio12 {
 
 	public static void main(String[] args) {
 		int evenLength = 10;
-		int oddLength = 5;
-		int resultLength = evenLength + oddLength;
-		
+		int[] evenArray = new int[evenLength];
 		int evenCount = 0;
-		int offCount = 0;
+		
+		int oddLength = 5;
+		int[] oddArray = new int[oddLength];
+		int oddCount = 0;
+		
+		int resultLength = evenLength + oddLength;
+		int[] resultArray = new int[resultLength];
+		
+		for (int i = 0; i <= evenLength * 2; i++) {
+			if (i % 2 == 0 && evenCount <= evenLength) {
+				evenArray[i] = i;
+				evenCount++;
+			}
+			if (i % 2 != 0 && oddCount <= oddLength) {
+				oddArray[i] = i;
+				oddCount++;
+			}
+			
+		}
 		
 	}
 
---
+++ src/paquete01/Tablero.java
@@ -0,0 +1,12 @@
+package paquete01;
+
+public class Tablero {
+
+	public static void main(String[] args) {
+		int[] numArray = { 1, 2, 3, 4, 5 };
+		int temp = 0;
+		
+		
+	}
+
+}
